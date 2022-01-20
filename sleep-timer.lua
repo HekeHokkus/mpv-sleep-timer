1b51d8e34994525b08880f817085de22412f694a
HekeHokkus <hekehokkus@gmail.com>
2022-01-20 16:26:37 -0500

    Update sleep-timer.lua

--- sleep-timer.lua
+++ sleep-timer.lua
@@ -2,15 +2,22 @@ local sleepAfter = 0
 local startSleep = nil
 
 function sleep()
+    if sleepAfter = 0 then
+        mp.add_timeout(60, sleepBegin)
+    end
     sleepAfter = sleepAfter + 15
-    startSleep = mp.add_timeout(sleepAfter * 60, sleepPause)
-    startSleep:resume()
     mp.osd_message("Sleep in "..sleepAfter.." minutes")
+end
+
+function sleepBegin()
+    sleepAfter = sleepAfter - 1
+    startSleep = mp.add_timeout(sleepAfter * 60, sleepPause)
 end
 
 function sleepPause()
     mp.set_property_native("pause", true)
     startSleep:kill()
+    startSleep = nil
     sleepAfter = 0
 end
 
