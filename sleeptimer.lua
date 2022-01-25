28a10ba902cb58a8f2a4d9b2d28744b2ea58417c
HekeHokkus <hekehokkus@gmail.com>
2022-01-25 08:02:18 -0500

    Update sleeptimer.lua
    Episode-counting functionality and some idiot-proofing

--- sleeptimer.lua
+++ sleeptimer.lua
@@ -3,14 +3,35 @@ local options = require "mp.options"
 local o = {
     increment = 15,
     minimum = 15,
-    maximum = 150
+    maximum = 150,
+    episodes = false
 }
 
 options.read_options(o, "sleeptimer")
 
+If o.episodes == true then
+    o.increment = 1
+    o.minimum = 1
+    o.maximum = 5
+    options.read_options(o, "sleeptimer")
+end
+
 local sleepAfter = o.minimum - o.increment
 local startSleep = nil
 local timeout = nil
+local timeron = false
+
+if o.increment < 1 then
+    o.increment = 1
+end
+
+if o.minimim < 1 then
+    o.minimum = 1
+end
+
+if o.maximum < o.minimum then
+    o.maximum = o.minimum
+end
 
 function sleep()
     if (sleepAfter == o.minimum - o.increment) then
@@ -47,4 +68,46 @@ function sleepPause()
     sleepAfter = o.minimum - o.increment
 end
 
-mp.add_key_binding("Ctrl+SPACE", "sleep-timer", sleep)
+function sleepEpisode()
+    if (sleepAfter == o.minimum - o.increment) then
+        timeout = mp.add_timeout(60, episodeBegin)
+        sleepAfter = (sleepAfter + o.increment)
+        mp.osd_message("Sleep in "..sleepAfter.." episodes")
+    elseif timeout == nil then
+        sleepAfter = o.minimum - o.increment
+        mp.osd_message("Sleep cancelled")
+        timeron = false
+    elseif (sleepAfter >= o.maximum) then
+        timeout:kill()
+        timeout = nil
+        sleepAfter = o.minimum - o.increment
+        mp.osd_message("Sleep cancelled")
+    else
+        sleepAfter = (sleepAfter + o.increment)
+        mp.osd_message("Sleep in "..sleepAfter.." episodes")
+    end
+end
+
+function episodeCount()
+    if timeron == true then
+        sleepAfter = sleepAfter - 1
+        if sleepAfter == 0 then
+            mp.set_property_native("pause", true)
+            sleepAfter = o.minimum - o.increment
+            timeron = false
+        end
+    end
+end
+
+function episodeBegin()
+    timeout:kill()
+    timeout = nil
+    timeron = true
+end
+
+if o.episodes == true then
+    mp.register_event("file-loaded", episodeCount)
+    mp.add_key_binding("Ctrl+SPACE", "sleep-timer", sleepEpisode)
+else
+    mp.add_key_binding("Ctrl+SPACE", "sleep-timer", sleep)
+end
